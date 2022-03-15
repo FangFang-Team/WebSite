8a285f28acbfd8db991f4637fdf913f6d6ed4c5b
mrsecond <906747215@qq.com>
2022-03-15 20:08:14 +0800

    3.15

--- part/src/App.vue
+++ part/src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <function></function>
+    <Home></Home>
   </div>
 </template>
 
 <script>
+import Home from './components/Home'
+
 export default {
-  name: 'App'
+  name: 'app',
+  components: {
+    Home,
+    function
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ part/src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="button">
+    <el-button type="text" @click="remote">遥感数据</el-button>
+    <el-button type="text">街景数据</el-button>
+    <el-button type="text">街景数据</el-button>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      value2: new Date(2016, 9, 10, 18, 40),
+      value3: new Date(2016, 9, 10, 18, 40)
+    }
+  },
+  methods: {
+      remote() {
+        this.$router.replace('/function')
+      }
+    }
+}
+</script>
+ 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button {
+  font-size: 20px;
+  margin: auto;
+}
+</style>
